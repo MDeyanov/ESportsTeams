67e913b0768847f5ff783f073f1195a0ec8f3246
MDeyanov <justdeyanov@icloud.com>
2022-12-15 02:37:04 +0200

    feat:unit_tests_fix

---
+++ ESportsTeams/ESportsTeams.Infrastructure/Data/Helpers/Html_String_Utility.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+
+namespace ESportsTeams.Infrastructure.Data.Helpers
+{
+    public class Html_String_Utility
+    {
+        public static string EncodeProperty(string property)
+        {
+            return HttpUtility.HtmlEncode(property);
+        }
+
+        public static string DecodeProperty(string property)
+        {
+            return HttpUtility.HtmlDecode(property);
+        }
+    }
+}
